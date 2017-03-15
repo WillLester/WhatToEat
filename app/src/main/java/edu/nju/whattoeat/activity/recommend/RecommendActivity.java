00e0a75faa16b83616f443152538b25024e9de75
Will Lester <839754614@qq.com>
2017-03-15 12:10:24 +0800

    update activities

---
+++ app/src/main/java/edu/nju/whattoeat/activity/recommend/RecommendActivity.java
@@ -0,0 +1,15 @@
+package edu.nju.whattoeat.activity.recommend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.nju.whattoeat.R;
+
+public class RecommendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend);
+    }
+}
---
+++ app/src/main/java/edu/nju/whattoeat/activity/user/PersonalInfoActivity.java
@@ -0,0 +1,15 @@
+package edu.nju.whattoeat.activity.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.nju.whattoeat.R;
+
+public class PersonalInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
